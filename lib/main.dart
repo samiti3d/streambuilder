da4e0a387ec72708ca4cdc4aef424bf57dc7d687
resmith <36131619+samiti3d@users.noreply.github.com>
2023-02-07 15:34:11 +0700

    add folder screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:streambuilder/screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Stream Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -19,74 +20,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  late Stream<int?> numbersStream;
-
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    numbersStream = getNumbers();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<int?>(
-        stream: numbersStream,
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              return const Text('Stream is null');
-            case ConnectionState.waiting:
-              return const Text('wait');
-            case ConnectionState.active:
-              if(snapshot.hasData) {
-                int number = snapshot.data!;
-                return Text('$number');
-              } else {
-                return const Text('No data!');
-              }
-            case ConnectionState.done:
-              if(snapshot.hasData) {
-                int number = snapshot.data!;
-                return Text('$number');
-              } else {
-                return const Text('No data!');
-              }
-            default:
-              return Container();
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.refresh),
-        onPressed: (){
-          setState(() {
-            numbersStream = getNumbers();
-          });
-        },
-      ),
-    );
-  }
-
-  Stream<int?> getNumbers() async* {
-    await Future.delayed(const Duration(seconds: 4));
-    yield 1;
-    await Future.delayed(const Duration(seconds: 1));
-    yield 2;
-    await Future.delayed(const Duration(seconds: 1));
-    yield 3;
-  }
-}
-
 
